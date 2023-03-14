e24669b77b8cb1df95e8164ba40f0f2608a4d6db
Jayti Gupta <jayti.gupta@centricconsulting.com>
2023-03-14 18:33:40 +0530

    adding comment

--- Base/basepage.py
+++ Base/basepage.py
@@ -9,6 +9,8 @@ class BasePage:
         self.driver.maximize_window()
         self.driver.get(self.url)
 
+    # To get the title of the page
     def get_title(self):
         return self.driver.title
+
 
