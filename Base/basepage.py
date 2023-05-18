28ae212b9ac5928a09f4c787a9b2c353bf62e776
shubham.singh <shubham.singh@centricconsulting.com>
2023-05-18 17:28:10 +0530

    Updated 'accept_alert' method in BasePage class

--- Base/basepage.py
+++ Base/basepage.py
@@ -1,7 +1,13 @@
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
 class BasePage:
 
     def __init__(self, driver, url):
         self.driver = driver
+        self.driver.set_page_load_timeout(5)
         self.url = url
 
     def go(self):
@@ -11,5 +17,14 @@ class BasePage:
     # To get the title of the page
     def get_title(self):
         return self.driver.title
+
+    def accept_alert(self):
+        try:
+            WebDriverWait(self.driver, 3).until(EC.alert_is_present())
+            alert = self.driver.switch_to.alert
+            alert.accept()
+            alert.accept()
+        except:
+            pass
 
 
