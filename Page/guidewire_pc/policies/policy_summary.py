b41436776e982827691f38c878e71525ed539ca1
shubham.singh <shubham.singh@centricconsulting.com>
2023-06-13 20:46:43 +0530

    small update

--- Page/guidewire_pc/policies/policy_summary.py
+++ Page/guidewire_pc/policies/policy_summary.py
@@ -9,3 +9,17 @@ class PolicySummary(BasePage):
 
     def __init__(self, driver):
         super().__init__(driver=driver, url=None)
+
+    @property
+    def _new_transaction_button(self):
+        locator = (By.XPATH, '//div[text()="New Transaction"]')
+        return BaseElement(self.driver, locator)
+
+    def _transaction_type(self, transaction):
+        locator = (By.XPATH, f'//div[text()="New Transaction"]/parent::div/'
+                             f'following-sibling::div[@role="menu"]//div[text()="{transaction}"]')
+        return BaseElement(self.driver, locator)
+
+    def select_transaction(self, transaction):
+        self._new_transaction_button.click_element()
+        self._transaction_type(transaction).click_element()
