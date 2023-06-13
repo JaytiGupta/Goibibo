2ae4e33da80e7d76150bffb2b3ae8a751592588b
shubham.singh <shubham.singh@centricconsulting.com>
2023-06-13 19:51:37 +0530

    Added PolicyInfo Class and updated Forms class

--- Page/guidewire_pc/policies/LOBs/common.py
+++ Page/guidewire_pc/policies/LOBs/common.py
@@ -2,6 +2,7 @@ import time
 from re import sub
 from selenium.webdriver.common.by import By
 from Base.baseelement import BaseElement
+from Base.baseelement import NestedElement
 from Base.basepage import BasePage
 from Util.logs import getLogger
 
@@ -200,33 +201,82 @@ class Workspace:
         return BaseElement(self.driver, locator)
 
 
+class PolicyInfo(BasePage):
+    log = getLogger()
+
+    def __init__(self, driver):
+        super().__init__(driver=driver, url=None)
+        self.SCREEN_TITLE = "Policy Info"
+        self._locator_FEIN_input_box = (By.XPATH, '//div[text()="FEIN"]/parent::div//input')
+        self._locator_industry_code_input_box = (By.XPATH, '//div[text()="Industry Code"]/parent::div//input')
+        self._locator_year_business_started_input_box = (
+            By.XPATH, '//div[text()="Year Business Started"]/parent::div//input')
+        self._locator_organization_type_dropdown = (By.XPATH, '//div[text()="Organization Type"]/parent::div//select')
+        self._locator_term_type_dropdown = (By.XPATH, '//div[text()="Term Type"]/parent::div//select')
+        self._locator_effective_date_input_box = (By.XPATH, '//div[text()="Effective Date"]/parent::div//input')
+        self._locator_underwriter_companies_dropdown = (By.XPATH, '//select[contains(@name, "UWCompanyInputSet")]')
+
+    def input_FEIN(self, text):
+        fein = BaseElement(self.driver, self._locator_FEIN_input_box)
+        fein.enter_text(text)  # text must be 9-digit no.
+        self.log.info(f"Enter FEIN - {text}")
+
+    def industry_code_input(self, industry_code):
+        industry_code_elm = BaseElement(self.driver, self._locator_industry_code_input_box)
+        industry_code_elm.enter_text(industry_code)
+        self.log.info(f"Enter Industry Code - {industry_code}")
+
+    def select_org_type(self, type_of_org):
+        org_type_elm = BaseElement(self.driver, self._locator_organization_type_dropdown)
+        org_type_elm.select_option(text=type_of_org)
+        self.log.info(f"Select Organisation Type - {type_of_org}")
+
+    def term_type(self, pol_term):
+        term_type_elm = BaseElement(self.driver, self._locator_term_type_dropdown)
+        term_type_elm.select_option(text=pol_term)
+        self.log.info(f"Select Policy Term Type - {pol_term}")
+
+    def policy_effective_date(self, policy_date):
+        eff_date_elm = BaseElement(self.driver, self._locator_effective_date_input_box)
+        eff_date_elm.enter_text(policy_date)
+        self.log.info(f"Enter Policy Effective Date - {policy_date}")
+
+    def uw_companies(self, uw_company):
+        uw_company_elm = BaseElement(self.driver, self._locator_underwriter_companies_dropdown)
+        uw_company_elm.select_option(text=uw_company)
+        self.log.info(f"Select Underwriting company - {uw_company}")
+
+
 class RiskAnalysis(BasePage):
     log = getLogger()
 
     def __init__(self, driver):
         super().__init__(driver=driver, url=None)
         self.SCREEN_TITLE = "Risk Analysis"
-        self._locator_all_pending_approval_check_box = (
-            By.XPATH, '//div[text()="Approve"]/ancestor::td[not(contains(@colspan,"1"))]'
-                      '/preceding-sibling::td[4]//input[@type="checkbox"]')
-        self._locator_header_approve_btn = (By.XPATH, '//div[@class="gw-ListView--UI-left"]'
-                                                       '//div[text()="Approve"]')
 
-        # Screen: Risk Approval Details
-        self._locator_ok_btn = (By.XPATH, '//div[@role="button"]//div[@aria-label="OK"]')
+    @property
+    def _all_pending_approval_check_box(self):
+        locator = (By.XPATH, '//div[text()="Approve"]/ancestor::td[not(contains(@colspan,"1"))]/preceding-sibling::td[4]//input[@type="checkbox"]')
+        return BaseElement(self.driver, locator)
 
-    # TODO: select only the required check boxes
+    @property
+    def _header_approve_btn(self):
+        locator = (By.XPATH, '//div[@class="gw-ListView--UI-left"]//div[text()="Approve"]')
+        return BaseElement(self.driver, locator)
+
+    @property
+    def _risk_approval_details_screen_ok_button(self):
+        locator = (By.XPATH, '//div[@role="button"]//div[@aria-label="OK"]')
+        return BaseElement(self.driver, locator)
+
     def approve_all_uw_issues(self):
-        all_check_box = BaseElement(self.driver, self._locator_all_pending_approval_check_box)
-        all_check_box.click_all_elements()
-        header_approve_btn = BaseElement(self.driver, self._locator_header_approve_btn)
-        header_approve_btn.click_element()
-
-        risk_approval_screen_ok_btn = BaseElement(self.driver, self._locator_ok_btn)
-        risk_approval_screen_ok_btn.click_element()
-
+        self._all_pending_approval_check_box.click_all_elements()
+        self._header_approve_btn.click_element()
+        self._risk_approval_details_screen_ok_button.click_element()
         self.accept_alert()
-        if all_check_box.is_element_present():
+
+        pending_check_boxes = self._all_pending_approval_check_box.is_element_present()
+        if pending_check_boxes:
             self.log.debug("All UW Issues are not approved yet.")
         else:
             self.log.info("All UW Issues are approved.")
@@ -246,14 +296,16 @@ class Quote(BasePage):
     def __init__(self, driver):
         super().__init__(driver=driver, url=None)
         self.SCREEN_TITLE = "Quote"
-        self._locator_total_premium_amt = (By.XPATH, '//div[text()="Total Premium"]'
-                                                     '/following-sibling::div//div[contains(text(), "$")]')
 
-    def total_premium_amt(self):
-        elm = BaseElement(self.driver, self._locator_total_premium_amt)
-        premium_txt =  elm.get_text()
-        premium = float(sub(r'[^\d.]', '', premium_txt))
-        self.log.info(f"Total Premium is {premium_txt}")
+    @property
+    def _total_premium_amt(self):
+        locator = (By.XPATH, '//div[text()="Total Premium"]/following-sibling::div//div[contains(text(), "$")]')
+        return BaseElement(self.driver, locator)
+
+    def total_premium_amount(self):
+        premium_amount_text =  self._total_premium_amt.get_text()
+        premium = float(sub(r'[^\d.]', '', premium_amount_text))
+        self.log.info(f"Total Premium is {premium_amount_text}")
         return premium
 
 
@@ -263,14 +315,35 @@ class Forms(BasePage):
     def __init__(self, driver):
         super().__init__(driver=driver, url=None)
         self.SCREEN_TITLE = "Forms"
-        # self._locator_s = (By.XPATH, '//div[text()="Form #"]/ancestor::tr/following-sibling::tr')
-        self._locator_ph_notice = (By.XPATH, '//div[contains(text(),"Notice to Policyholders")]')
 
-    def ph_notice(self):
-        ph_notice = BaseElement(self.driver, self._locator_ph_notice)
-        if ph_notice.get_text() == "Notice to Policyholders":
-            print("Notice to Policyholders form is generated")
-            self.log.info("Policy Forms are generated")
-        else:
-            print("PH Notice is not generated for the quote")
-            self.log.info("Forms are not generated correctly")
+    @property
+    def all_form_rows(self):
+        locator = (By.XPATH, '//table//tr[1]/following-sibling::tr')
+        return BaseElement(self.driver, locator)
+
+    def all_form_list(self):
+        """
+        :return: list of all forms, where each form is a dictionary contains 'Form#' and 'Description'
+        """
+        form_details_list = []
+        all_form_row_elements = self.all_form_rows.get_all_elements()
+
+        for form_row_element in all_form_row_elements:
+            """
+            If you start an XPath expression with //, it begins searching from the root of document.
+            To search relative to a particular element, you should prepend the expression with '.' instead
+            """
+            form_number_locator = (By.XPATH, './/div[contains(@id, "FormNumber")]')
+            form_description_locator = (By.XPATH, './/div[contains(@id, "Description")]')
+            form_number = NestedElement(form_row_element, form_number_locator).get_text()
+            form_description = NestedElement(form_row_element, form_description_locator).get_text()
+
+            form_dict = {
+                    "Form#": form_number,
+                    "Description": form_description,
+                }
+
+            form_details_list.append(form_dict)
+
+        return form_details_list
+
