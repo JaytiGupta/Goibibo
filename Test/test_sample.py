3b3eadc85439121f2aa9be4dd572b96b9b5a254f
Jayti Gupta <jayti.gupta@centricconsulting.com>
2023-07-26 13:43:42 +0530

    26th - Jayti

--- Test/test_sample.py
+++ Test/test_sample.py
@@ -1,7 +1,8 @@
 from Util.logs import getLogger
-from definitions import global_dict
+from pytest import mark
 
 
+@mark.skip
 def test_new(env, take_screenshots, guidewire):
     print(f"\nEnvironment: {env}")
     print(f"Taking Screenshots: {take_screenshots} ")
