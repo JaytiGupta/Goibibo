53d60c6818098a63f26b7060f6496f6b561dfdf3
shubham.singh <shubham.singh@centricconsulting.com>
2023-07-27 18:07:43 +0530

    27th July

--- Test/guidewire/test_home.py
+++ Test/guidewire/test_home.py
@@ -9,8 +9,10 @@ from Page.guidewire_pc.policies.common.sidebar import Sidebar
 # @mark.regression
 # @mark.smoke
 def test_search_account(browser_pc):
+    account_number ="3164631717"
+    # account_number ="5635397383"
     page = PolicyCenterHome(browser_pc)
-    page.tab_bar.search_account("5635397383")
+    page.tab_bar.search_account(account_number)
     account = Account(browser_pc)
     assert account.summary.account_summary_title_present()
     Screenshot.capture(browser_pc)
@@ -19,7 +21,8 @@ def test_search_account(browser_pc):
 # @mark.smoke
 # @mark.regression
 def test_search_submission(browser_pc):
-    submission_number = "0000038964"
+    submission_number = "0000655199"
+    # submission_number = "0000038964"
     page = PolicyCenterHome(browser_pc)
     page.tab_bar.search_submission(submission_number)
     policy_sidebar = Sidebar(browser_pc)
@@ -30,7 +33,8 @@ def test_search_submission(browser_pc):
 # @mark.smoke
 # @mark.regression
 def test_search_policy(browser_pc):
-    policy_number_to_search = "8410742626"
+    policy_number_to_search = "2672994591"
+    # policy_number_to_search = "8410742626"
     page = PolicyCenterHome(browser_pc)
     page.tab_bar.search_policy(policy_number_to_search)
     policy_summary_screen = PolicySummary(browser_pc)
